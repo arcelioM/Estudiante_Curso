5349a547244f8f69c5ea27428b2ed9065dcc1789
arcelioM <arceliomonte15@gmail.com>
2022-10-20 09:46:09 -0500

    Entidad de Estudiante agregado a model

---
+++ src/estudiante_calificaciones/model/Estudiante.java
@@ -0,0 +1,85 @@
+
+package estudiante_calificaciones.model;
+
+import java.time.LocalDate;
+
+public class Estudiante {
+    
+    private Integer id;
+    private Integer cedula;
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+    private LocalDate fechaCreacion;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(Integer id) {
+        this.id = id;
+    }
+
+    public Estudiante(Integer id, Integer cedula, String nombre, String apellido, Integer edad, LocalDate fechaCreacion) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(Integer cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "id=" + id + ", cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", fechaCreacion=" + fechaCreacion + '}';
+    }
+    
+    
+}
