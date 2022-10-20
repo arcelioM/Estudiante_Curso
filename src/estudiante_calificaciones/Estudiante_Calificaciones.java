647e9150e35257bee2a9f1260a640e4dc18215d3
arcelioM <arceliomonte15@gmail.com>
2022-10-19 23:47:25 -0500

    Inicio de proyecto

---
+++ src/estudiante_calificaciones/Estudiante_Calificaciones.java
@@ -0,0 +1,13 @@
+package estudiante_calificaciones;
+
+
+public class Estudiante_Calificaciones {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
