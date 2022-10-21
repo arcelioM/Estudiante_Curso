fac456b223a48ace41d46b3047e4f3b4bdc2d735
arcelioM <arceliomonte15@gmail.com>
2022-10-20 21:08:19 -0500

    Metodo de vista para actualizacion de estudiante creada

--- src/estudiante_calificaciones/vista/EstudianteVista.java
+++ src/estudiante_calificaciones/vista/EstudianteVista.java
@@ -35,5 +35,61 @@ public class EstudianteVista {
     }
     
     
-    
+    public Integer actualizar(Estudiantes estudiante){
+        
+        System.out.println("Actualizando datos");
+        System.out.println("------------------------------------------------- \n\n");
+        
+        System.out.println("Datos actuales de estudiante..");
+        System.out.print("1 - Cedula: "+estudiante.getCedula());
+        System.out.print(" | 2 - Nombre: "+estudiante.getNombre());
+        System.out.print(" | 3 - Apellido: "+estudiante.getApellido());
+        System.out.println(" | 4 - Edad: "+estudiante.getEdad()+" \n");
+       
+        System.out.println("Escriba el numero del dato que desea actualizar..... \n\n");
+        Integer seleccion=1;
+        
+        while (seleccion>0) { 
+            System.out.println("Escriba el numero del dato que desea actualizar (0 para salir)..... \n\n");
+            seleccion=sc.nextInt();
+            
+            switch (seleccion) {
+                
+                case 0:
+                    System.out.println("Saliendo...............");
+                    break;
+                
+                case 1:
+                    System.out.print("Cedula: ");
+                    Integer cedula=sc.nextInt();
+                    estudiante.setCedula(cedula);
+                    sc.nextLine();
+                    break;
+                    
+                case 2:
+                    System.out.print("Nombre: ");
+                    String nombre=sc.nextLine();
+                    estudiante.setNombre(nombre);
+                    break;
+                    
+                case 3: 
+                    System.out.print("Apellido: ");
+                    String apellido=sc.nextLine();
+                    estudiante.setApellido(apellido);
+                    break;
+                    
+                case 4:
+                    System.out.print("Edad: ");
+                    Integer edad=sc.nextInt();
+                    sc.nextLine();
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }
+        
+        Integer filas=estudianteDao.update(estudiante);
+        
+        return filas;
+    }
 }
